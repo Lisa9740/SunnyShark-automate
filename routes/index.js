67c515b85c5e27e08ae00f8106775e415f2cf8ff
Lisa9740 <barretalison@gmail.com>
2018-10-14 20:56:52 +0400

    mise en place des routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var automateController = require("../controllers/automateController.js");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gestion des Automates' });
-});
+
+// Get all automateControllers
+router.get('/', automateController.list);
+
+// Get single automateController by id
+router.get('/show/:id', automateController.show);
+
+// Create automateController
+router.get('/create', automateController.create);
+
+// Save automateController
+ router.post('/save', automateController.save);
+
+// Edit automateController
+router.get('/edit/:id', automateController.edit);
+
+// Edit update
+router.post('/update/', automateController.update);
+
+// Edit update
+router.post('/delete/:id', automateController.delete);
+
+module.exports = router;
+
 
 module.exports = router;
